79916c750b23b22206e464bdec6b5f0c33920f82
C. T. Lin <chentsulin@gmail.com>
2016-06-20 16:48:37 +0800

    import serve from top of server file

--- app/templates/server.js
+++ app/templates/server.js
@@ -4,6 +4,7 @@ import logger from 'koa-logger';
 import session from 'koa-session';
 import views from 'koa-views';
 import convert from 'koa-convert';
+import serve from 'koa-static';
 import finalHandler from './lib/middlewares/finalHandler';
 import router from './router';
 
@@ -19,7 +20,7 @@ app.use(logger());
 app.use(bodyParser());
 app.keys = ['some secret hurr'];
 app.use(convert(session(app)));
-app.use(convert(require('koa-static')(__dirname + '/public')));
+app.use(serve(__dirname + '/public'));
 app
   .use(router.routes())
   .use(router.allowedMethods());
